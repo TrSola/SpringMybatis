683c1107388de9e10c057504e2b842f2df23c761
Willy <willy718066@gmail.com>
2024-09-01 18:22:15 +0800

    feat 第八篇筆記內容更新
    Service改為接收物件
    
    by Willy 2024/9/1 18:22

--- src/main/java/com/willy/mapper/DeptMapper.java
+++ src/main/java/com/willy/mapper/DeptMapper.java
@@ -1,8 +1,16 @@
 package com.willy.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import com.willy.pojo.Dept;
 
 @Mapper
 public interface DeptMapper {
+
+	@Select("select * from dept")
+	List<Dept> list();
 
 }
